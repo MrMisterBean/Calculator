77259fcf1b75cb9ad602c498ed188e1f36ba44ad
MrMisterBean <alutter03@gmail.com>
2022-05-06 00:45:29 -0400

    3x3 grid of numbers 1-9

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 const Home = () => {
 
   const Grid = () => [...Array(9).keys()].map(i => {
-    return <div className="text-light"> {i + 1} </div>
+    return <div className="flex items-center justify-center h-full bg-dark text-light">{i + 1}</div>
   })
 
   return (
-    <div className="grid grid-flow-row grid-cols-3 text-light">
-      <Grid />
+    <div className="flex h-screen p-8 border-2 place-items-center">
+      <div className="grid w-full h-full grid-flow-row border-light grid-cols-3 gap-[2px] text-center border-2 bg-light text-light">
+        <Grid />
+      </div>
     </div>
   )
 
