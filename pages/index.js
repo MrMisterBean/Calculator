4af77efa8f91be3bd00e3cc339bd800daa5d5bb8
MrMisterBean <alutter03@gmail.com>
2022-05-08 22:12:29 -0400

    added secondary text for previous input

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ const Home = () => {
 
   return (
     <div className="grid h-screen grid-flow-row p-8 border-2 grid-row-6 place-items-center">
+
       {/* <textarea className="w-full row-span-1 text-6xl text-right h-2/3" /> */}
-      <p className="w-full row-span-1 text-6xl text-right bg-light h-2/3" contentEditable="true"> 965 </p>
-      <div className="grid w-full row-span-5 h-full grid-flow-row border-light grid-cols-3 grid-rows-3 gap-[2px] text-center border-2 bg-light text-light">
+      <p className="relative w-full row-span-1 text-right bg-light h-2/3" contentEditable="true">
+        <span className="absolute top-0 right-0 px-4 py-2 text-2xl">66936 +</span>
+        <span className="absolute bottom-0 right-0 px-4 py-4 text-5xl">09354</span>
+      </p>
+      <div className="grid w-full grid-re row-span-5 h-full grid-flow-row border-light grid-cols-3 grid-rows-3 gap-[2px] text-center border-2 bg-light text-light">
         <Grid />
       </div>
     </div>
